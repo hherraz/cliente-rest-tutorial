b9b00c1d0803e453717d16b077fc7695fa7efa92
Heber Herraz <heber.herraz@gmail.com>
2018-01-14 17:45:07 -0300

    Agregando el routing a  nuestro proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -14,7 +17,10 @@ import { HeroeComponent } from './components/heroes/heroe.component';
     HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
